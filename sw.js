42472db02240949d3cdaf6f8210c31bfb32fb8dd
esp32mailnotif-eng <esp32mailnotif@gmail.com>
2026-01-05 17:23:28 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,15 @@
-const CACHE_NAME = 'wake-v1';
-const ASSETS = [
-  'index.html',
-  'manifest.json'
-];
+const CACHE_NAME = 'wake-v2'; // Changed version to force update
+const ASSETS = ['index.html', 'manifest.json'];
 
-// Install Service Worker
 self.addEventListener('install', (e) => {
+  self.skipWaiting(); // Force the new service worker to take over immediately
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Fetch Assets from Cache
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
