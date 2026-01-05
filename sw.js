125a1707a800d6b1ae2f78a11ea8ba1a82998a33
esp32mailnotif-eng <esp32mailnotif@gmail.com>
2026-01-05 14:47:02 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,14 @@
-const CACHE_NAME = 'offline-wake-v1';
-const ASSETS = ['./index.html'];
+const CACHE = 'c3-wake-v1';
 
-self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
-});
-
-self.addEventListener('fetch', (e) => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c =>
+      c.addAll([
+        './',
+        './index.html',
+        './manifest.webmanifest'
+      ])
+    )
+  );
+  self.skipWaiting();
 });
