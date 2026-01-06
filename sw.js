be4621880f81574ff09bd242a57538414bf521a9
esp32mailnotif-eng <esp32mailnotif@gmail.com>
2026-01-06 15:30:02 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,50 +1,22 @@
-// 1. Update this version number every time you change index.html
-const cacheName = 's3-wake-pro-v2.7';
+const cacheName = 's3-v2.9-dual-timer';
+const assets = ['./', './index.html', './manifest.json'];
 
-// 2. Files to store for offline use (App Shell)
-const assets = [
-  './',
-  './index.html',
-  './manifest.json',
-  // Add icon paths here if you have them, e.g., './icon.png'
-];
-
-// INSTALL: Pre-cache the app files
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      console.log('S3 Wake Pro: Pre-caching assets');
-      return cache.addAll(assets);
-    })
-  );
-  // Force the new service worker to become active immediately
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(cacheName).then((c) => c.addAll(assets)));
   self.skipWaiting();
 });
 
-// ACTIVATE: Cleanup old caches from previous versions
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
     caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== cacheName) {
-            console.log('S3 Wake Pro: Deleting old cache', key);
-            return caches.delete(key);
-          }
-        })
-      );
+      return Promise.all(keys.map((k) => {
+        if (k !== cacheName) return caches.delete(k);
+      }));
     })
   );
-  // Take control of all open tabs immediately
   return self.clients.claim();
 });
 
-// FETCH: Serve from cache first, then network (Offline-First)
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      // Return cached file if found, otherwise fetch from internet
-      return cachedResponse || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
