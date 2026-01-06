d941e0830cd828ea8dc81e094717dbf123cdbb32
esp32mailnotif-eng <esp32mailnotif@gmail.com>
2026-01-06 14:58:19 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,50 @@
-const cacheName = 's3-v2.5-heartbeat'; 
+// 1. Update this version number every time you change index.html
+const cacheName = 's3-wake-pro-v2.7';
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(cacheName).then((cache) => cache.addAll(['index.html', 'manifest.json']))
+// 2. Files to store for offline use (App Shell)
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  // Add icon paths here if you have them, e.g., './icon.png'
+];
+
+// INSTALL: Pre-cache the app files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      console.log('S3 Wake Pro: Pre-caching assets');
+      return cache.addAll(assets);
+    })
   );
-  self.skipWaiting(); 
+  // Force the new service worker to become active immediately
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+// ACTIVATE: Cleanup old caches from previous versions
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== cacheName) {
+            console.log('S3 Wake Pro: Deleting old cache', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  // Take control of all open tabs immediately
+  return self.clients.claim();
+});
+
+// FETCH: Serve from cache first, then network (Offline-First)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      // Return cached file if found, otherwise fetch from internet
+      return cachedResponse || fetch(event.request);
+    })
   );
 });
